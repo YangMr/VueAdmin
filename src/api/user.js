ff028d4d17b839ef5e60ebdb14fa0008e4b89b3d
YangMr <759168442@qq.com>
2022-07-06 09:58:52 +0800

    feat(跨域、api、request): 配置跨域以及axios与api封装

---
+++ src/api/user.js
@@ -0,0 +1,41 @@
+import request from '../utils/request'
+
+/**
+ * 获取验证码接口
+ * @returns
+ */
+const getCaptcha = () => {
+  return request({ url: '/captcha', method: 'GET' })
+}
+
+/**
+ * 登录接口
+ * @param {*} data
+ * @returns
+ */
+const login = (data) => {
+  return request({ url: `/login?username=${data.username}&password=${data.password}&code=${data.code}&token=${data.token}`, method: 'POST' })
+}
+
+/**
+ * 获取用户信息接口
+ * @returns
+ */
+const getUserInfo = () => {
+  return request({ url: '/sys/userInfo', method: 'GET' })
+}
+
+/**
+ * 退出登录接口
+ * @returns
+ */
+const logout = () => {
+  return request({ url: '/logout', method: 'POST' })
+}
+
+export default {
+  getCaptcha,
+  login,
+  getUserInfo,
+  logout
+}
